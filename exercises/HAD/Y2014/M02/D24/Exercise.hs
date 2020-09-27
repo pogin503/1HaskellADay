fd5bc099183bd1b88641a316f6f7f0788501f7d1
pogin503 <pogin503@gmail.com>
2020-09-27 18:40:26 +0900

    2014 02 24

--- exercises/HAD/Y2014/M02/D24/Exercise.hs
+++ exercises/HAD/Y2014/M02/D24/Exercise.hs
@@ -1,5 +1,4 @@
 module HAD.Y2014.M02.D24.Exercise where
-
 -- | Filter a list, keeping an element only if it is equal to the next one.
 --
 -- Examples:
@@ -11,4 +10,25 @@ module HAD.Y2014.M02.D24.Exercise where
 -- [2,2,3]
 
 --filterByPair :: Find the most generic signature
-filterByPair = undefined
+-- OK
+filterByPair :: Eq a => [a] -> [a]
+filterByPair xs =
+  f xs
+  where
+    f [] = []
+    f [_] = []
+    f (x:y:xs') = if x == y
+                 then x : f (y:xs')
+                 else f (y:xs')
+
+-- TODO
+-- filterByPair' :: Eq a => [a] -> [a]
+-- filterByPair' xs =
+--   f xs
+--   where
+--     f xs'
+--       | xs' == [] = []
+--       | length xs' == 1 = xs'
+--       | otherwise = if xs' !! 0 == xs' !! 1
+--                     then xs' !! 0 : f (tail xs')
+--                     else f (tail xs')
